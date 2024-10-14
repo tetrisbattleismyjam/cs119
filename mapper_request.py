e59deb706ac70663fb8969b80a57143ae12f74cf
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 10:51:34 -0700

    Add files via upload

---
+++ mapper_request.py
@@ -0,0 +1,17 @@
+import sys
+
+def main(argv):
+    line = sys.stdin.readline()
+    request_index = 5
+
+    try:
+        while line:
+            request = line.split()[request_index].lstrip("\"")
+            print ("LongValueSum:" + request + "\t" + "1")
+            line = sys.stdin.readline()
+
+    except EOFError as error:
+        return None
+    
+if __name__ == "__main__":
+    main(sys.argv)
