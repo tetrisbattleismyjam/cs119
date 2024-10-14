265b3cccacdfd7e339207636d3f2a2613b5a7b0b
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 12:24:58 -0700

    Add files via upload

--- mapper_request.py
+++ mapper_request.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
 import sys
 
 def main(argv):
     line = sys.stdin.readline()
-    line = sys.stdin.readline() # dump empty first line
-    request_index = 5 # after splitting line, request code is in 5
+    request_index = 5
 
     try:
         while line:
-            request = line.split()[request_index][1:]
-            print ("LongValueSum:" + request.lower() + "\t" + "1")
+            request = line.split()[request_index]
+            print ("LongValueSum:" + request + "\t" + "1")
             line = sys.stdin.readline()
 
     except EOFError as error:
