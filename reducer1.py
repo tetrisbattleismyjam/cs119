f2405fbff88b79c4721867dd7c6752b0c2370c1c
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 18:37:59 -0700

    Update reducer1.py

--- reducer1.py
+++ reducer1.py
@@ -3,20 +3,20 @@
 import collections
 import sys
 
-def main(argv):
-  word_count = collections.Counter()
 
-  for line in sys.stdin:
-    line_ = line.strip()
-    word, count = line_.split('\t')
+word_count = collections.Counter()
 
-    try:
-      count = int(count)
-      word_count.update({word: count})
-    except:
-      continue # ignore this line silently
-      
-  calculatePercentages(word_count)
+for line in sys.stdin:
+  line_ = line.strip()
+  word, count = line_.split('\t')
+
+  try:
+    count = int(count)
+    word_count.update({word: count})
+  except:
+    continue # ignore this line silently
+    
+calculatePercentages(word_count)
 
 def calculatePercentages(word_count):
   total_count = word_count.total()
@@ -24,6 +24,3 @@ def calculatePercentages(word_count):
   for word in word_count:
     ratio = word_count[word] / total_count
     print(word + '\t' + ratio)
-
-if __name__ == "__main__":
-  main(sys.argv)
