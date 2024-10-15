ec077bb29ea75f932e38f51951e0fe1600cb99ae
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-15 16:18:31 -0700

    Update mapper_speeches1.py

--- mapper_speeches1.py
+++ mapper_speeches1.py
@@ -8,7 +8,16 @@ import collections
 
 stopwords_list = requests.get("https://gist.githubusercontent.com/rg089/35e00abf8941d72d419224cfd5b5925d/raw/12d899b70156fd0041fa9778d657330b024b959c/stopwords.txt").content
 stopwords = list(set(stopwords_list.decode().splitlines()))
+valence_list = requests.get("https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-en-165.txt").text
+valency = {}
+for line in valence_list.split('\n'):
+  try:
+    word, value = line.split('\t')
+    valency[word] = int(value)
+  except:
+    pass
 
+                   
 def main(argv):
     filename = os.environ['map_input_file']
     prez = filename.split('_')[0]
@@ -16,7 +25,18 @@ def main(argv):
     while line:
         phrase = clean_text(line.split())
         print(prez + '\t' + phrase)
+
+def valence(text):
+    return calc_valence(clean_text(text))
+
+def calc_valence(text):
+    valence = 0
+    for word in text.split(' '):
+        valence += valency.get(word, default=0)
         
+    return valence
+        
+    
 # Used in clean_text to remove stopwords.
 def remove_stopwords(words):
     list_ = re.sub(r"[^a-zA-Z0-9]", " ", words.lower()).split()
