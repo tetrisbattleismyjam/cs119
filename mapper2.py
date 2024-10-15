759e1411fa642b035d3593acafee14919ba19e59
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 21:26:48 -0700

    Update mapper2.py

--- mapper2.py
+++ mapper2.py
@@ -8,9 +8,28 @@ def main(argv):
     
     try:
         while line:
-            code = line.split()[code_index]
-            print (code + "\t" + "1")
-            line = sys.stdin.readline()
+          code = int(line.split()[code_index])
+
+          if code >= 100 and code < 200:
+            response = 'informational'
+            
+          elif code >= 200 and code < 300:
+            response = 'successful'
+            
+          elif code >= 300 and code < 400:
+            response = 'redirection'
+            
+          elif code >= 400 and code < 500:
+            response = 'client error'
+            
+          elif code >= 500 and code < 600:
+            response = 'server error'
+    
+          else:
+            response = 'BADLINE'       
+              
+        print (response + "\t" + "1")
+        line = sys.stdin.readline()
 
     except EOFError as error:
         return None
