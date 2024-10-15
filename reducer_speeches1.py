82323ed6c3a1eabc7410d784a4809a133a524815
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-15 16:31:18 -0700

    Update reducer_speeches1.py

--- reducer_speeches1.py
+++ reducer_speeches1.py
@@ -1,5 +1,13 @@
-def main(argv):
+import collections
+import sys
 
+def main(argv):
+  line = sys.stdin.readline()
+  counter = collections.Counter()
+  while line:
+    prez, valence, word_count = line.split('\t')
+    average = int(valence) / int(word_count)
+    counter.update({prez, average})
   
 if __name__ == "__main__":
   main(sys.argv)f
