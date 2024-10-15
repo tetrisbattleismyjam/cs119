fa5957a7210e03188118793c1237e249dd0e4630
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 19:46:59 -0700

    Create postprocess1.py

---
+++ postprocess1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def main(argv):
+  filepath = argv[1]
+  file_ = open(filepath)
+  for line in file.readline()
+    print(line)
+    
+if __name__ == "__main__":
+  main(sys.argv)
