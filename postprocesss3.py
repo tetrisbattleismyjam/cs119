302308adf1346edae6c3d270c26ea0f59def488a
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-15 10:43:05 -0700

    Update postprocesss3.py

--- postprocesss3.py
+++ postprocesss3.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import sys
 import collections
+import np
 
 def main(argv):
   filepath = argv[1]
   file_ = open(filepath)
   line = file_.readline()
   counts = []
+  total = 0
   
   while line:
     key, count = line.split('\t')
     count = int(count)
+    total += count
     counts.append((key, count))
     line = file_.readline()
 
   sorted_counts = sorted(counts, key=lambda a: a[1], reverse=True)
-  print(sorted_counts[:5])
+  print("top 5: ", sorted_counts[:5])
+  print("total client errors: ", total)
+  
 if __name__ == "__main__":
   main(sys.argv)
