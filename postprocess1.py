d4a8a2295fde30467e7ea3cfb1a8a9c5b5883182
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 20:04:58 -0700

    Update postprocess1.py

--- postprocess1.py
+++ postprocess1.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import sys
 
-
 def main(argv):
   filepath = argv[1]
   file_ = open(filepath)
-  for line in file_.readline():
-    print(line)
-    
+  line = file_.readline()
+  total_count = 0
+  word_counts = {}
+  
+  while line:
+    word, count = line.split('\t')
+    total_count += count
+    word_counts[word] = count
+
+  for key in word_counts:
+    print(key, word_counts[key]/total_count
+          
 if __name__ == "__main__":
   main(sys.argv)
