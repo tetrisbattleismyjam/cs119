3ec35b557bb1476daca1601bbd6c6661d2054219
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-15 16:32:30 -0700

    Update reducer_speeches1.py

--- reducer_speeches1.py
+++ reducer_speeches1.py
@@ -8,6 +8,9 @@ def main(argv):
     prez, valence, word_count = line.split('\t')
     average = int(valence) / int(word_count)
     counter.update({prez, average})
-  
+
+  for prez in counter:
+    print(prez + '\t' + counter[prez])
+    
 if __name__ == "__main__":
   main(sys.argv)f
