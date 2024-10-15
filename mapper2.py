3a4280b47406a0d37b345b12a7220705b4ed3b21
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 21:34:58 -0700

    Update mapper2.py

--- mapper2.py
+++ mapper2.py
@@ -8,31 +8,29 @@ def main(argv):
     
     try:
         while line:
-          code = int(line.split()[code_index])
+            code = int(line.split()[code_index])
+            code = int(code)
 
-          if code >= 100 and code < 200:
-            response = 'informational'
-            
-          elif code >= 200 and code < 300:
-            response = 'successful'
-            
-          elif code >= 300 and code < 400:
-            response = 'redirection'
-            
-          elif code >= 400 and code < 500:
-            response = 'client error'
-            
-          elif code >= 500 and code < 600:
-            response = 'server error'
-    
-          else:
-            response = 'BADLINE'       
+            if code >= 100 and code < 200:
+                response = 'informational'
+            elif code >= 200 and code < 300:
+                response = 'successful'
+            elif code >= 300 and code < 400:
+                response = 'redirection'
+            elif code >=400 and code < 500:
+                response = 'client error'
+            elif code >=500 and code < 600:
+                response = 'server error'
+            else:
+                raise ValueError("bad response code")
               
         print (response + "\t" + "1")
         line = sys.stdin.readline()
 
     except EOFError as error:
         return None
+    except ValueError:
+        continue
     
 if __name__ == "__main__":
     main(sys.argv)
