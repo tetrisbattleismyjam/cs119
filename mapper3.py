4d220cca7b5c7241d2c57410648d821b5fd9822d
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-15 08:14:02 -0700

    Create mapper3.py

---
+++ mapper3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+def main(argv):
+    line = sys.stdin.readline()
+    line = sys.stdin.readline()
+    key_index = 1
+
+    try:
+        while line:
+            key = line.split()[key_index]
+            print (key + "\t" + "1")
+            line = sys.stdin.readline()
+
+    except EOFError as error:
+        return None
+    
+if __name__ == "__main__":
+    main(sys.argv)
