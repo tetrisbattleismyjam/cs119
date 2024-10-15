62cc225ca25edf4c4046cfec55814207c7dca95a
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-14 21:27:14 -0700

    Update reducer2.py

--- reducer2.py
+++ reducer2.py
@@ -8,30 +8,10 @@ def main(argv):
   
   for line in sys.stdin:
     line_ = line.strip()
-    code, count = line_.split('\t')
+    response, count = line_.split('\t')
   
     try:
-      code = int(code)
       count = int(count)
-
-      if code >= 100 and code < 200:
-        response = 'informational'
-        
-      elif code >= 200 and code < 300:
-        response = 'successful'
-        
-      elif code >= 300 and code < 400:
-        response = 'redirection'
-        
-      elif code >= 400 and code < 500:
-        response = 'client error'
-        
-      elif code >= 500 and code < 600:
-        response = 'server error'
-
-      else:
-        response = 'BADLINE'
-        
       code_count.update({response: count})
     except:
       continue # ignore this line silently
