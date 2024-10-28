1de90359b0281101aec549b0d8f29cc8e240c218
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-10-28 14:18:55 -0700

    Update click_feeder.py

--- quiz6/click_feeder.py
+++ quiz6/click_feeder.py
@@ -8,12 +8,12 @@ from click_gen import *
 max_evs = 6000         # Total number of events that will be generated. 
                        # If you want to make it run forever, set this number to like a million
 
-n_senders = 1000         # Number of senders
+n_senders = 6        # Number of senders
 n_queries = 60         # How many different queries (or messages) a sender can send
 
 dispatcher = Dispatcher()
 sender_names = shuffle(['sndr%04d' % i for i in range(n_senders)])
-senders = [dispatcher.add_sender(Sender(sender_name, n_queries, n_senders*0.0001, n_senders*0.0001)) for sender_name in sender_names]
+senders = [dispatcher.add_sender(Sender(sender_name, n_queries, n_senders*1.0, n_senders*1.0)) for sender_name in sender_names]
 counter = 0
 
 for ev in dispatcher.launch():
