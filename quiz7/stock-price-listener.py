89090ef8a681573f1f044c41e00865214f792f5e
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 13:35:14 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 
     aapl_10.writeStream\
         .queryName('aapl_10')\
-        .outputMode("complete")\
+        .outputMode("update")\
         .format("memory") \
         .start()
     
