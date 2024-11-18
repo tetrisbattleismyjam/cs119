b9eb601cdedf5332b06bba00d36d60d6e658b1d2
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 12:35:32 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
             .option('port', port)\
             .load()
     
-    lines_split = lines.select(sql_f.element_at(sql_f.split(lines.value, '[\t]'), 1).alias('date')\
+    lines_split = lines.select(sql_f.element_at(sql_f.substring(sql_f.split(lines.value, '[\t]'), 1), 1, 10).alias('date')\
                                ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 2).alias('AAPL')\
                                ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 3).alias('MSFT'))
 
@@ -40,11 +40,13 @@ if __name__ == "__main__":
     msft_stream = lines_split.select(sql_f.col('date'), sql_f.col('MSFT').alias('price'))
 
     aapl_10 = aapl_stream.withColumn('max_date', sql_f.col('date'))\
-                            .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 10))
+                            .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 10))\
+                            .groupBy(sql_f.col('date'))\
     
     query = aapl_10\
             .writeStream\
             .format("console")\
+            .outputMode('complete')\
             .option('truncate', False)\
             .start()
     
