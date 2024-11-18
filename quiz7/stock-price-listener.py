9a20355853e8a84b7735ae31e197c7ecc3d56a57
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 09:35:50 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -30,11 +30,10 @@ def main():
             .option('port', port)\
             .load()
     
-    lines_split = lines.select(sql_f.split(lines.value, '[\t]'))\
-                        .rdd\
-                        .flatmap(lambda a: a)\
-                        .toDF(schema=['date','AAPL','MSFT'])
-    
+    lines_split = lines.select(sql_f.element_at(sql_f.split(lines.value, '[\t]'), 0).alias('date')\
+                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 1).alias('AAPL')\
+                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 2).alias('MSFT')
+                               
     query = lines_split\
             .writeStream\
             .format("console")\
