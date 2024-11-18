b934d2f35fd764655eed03906509de5c7edba492
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 12:59:40 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -33,14 +33,24 @@ if __name__ == "__main__":
             .load()
     
     lines_split = lines.select(sql_f.substring(sql_f.element_at(sql_f.split(lines.value, '[\t]'), 1), 1, 10).alias('date')\
-                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 2).alias('AAPL')\
-                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 3).alias('MSFT'))
+                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 3).alias('AAPL')\
+                               ,sql_f.element_at(sql_f.split(lines.value, '[\t]'), 2).alias('MSFT'))
 
     aapl_stream = lines_split.select(sql_f.col('date'), sql_f.col('AAPL').alias('price'))
     msft_stream = lines_split.select(sql_f.col('date'), sql_f.col('MSFT').alias('price'))
 
     aapl_10 = aapl_stream.withColumn('max_date', sql_f.col('date'))\
                             .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 10))\
+                            .agg({'price': 'avg'})
+    aapl_40 = aapl_stream.withColumn('max_date', sql_f.col('date'))\
+                            .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 40))\
+                            .agg({'price': 'avg'})
+
+    msft_10 = msft_stream.withColumn('max_date', sql_f.col('date'))\
+                            .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 10))\
+                            .agg({'price': 'avg'})
+    msft_40 = msft_stream.withColumn('max_date', sql_f.col('date'))\
+                            .filter(sql_f.col('date') > sql_f.date_sub(sql_f.col('max_date'), 40))\
                             .agg({'price': 'avg'})
     
     query = aapl_10\
