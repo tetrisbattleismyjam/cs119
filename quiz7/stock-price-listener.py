0154af6ef4cdad129e72cc2da4265f1489f21d83
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 13:43:43 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     
     time.sleep(5)
     while True:
-        row = spark.sql('select * from aapl_10').collect()\
+        row = spark.sql('select * from aapl_10').collect()
         avg_date = row[1]['max(date)']
         a_avg_10 = row[1]['avg(price)']
         a_avg_40 = spark.sql('select * from aapl_40').collect()[1]['avg(price)']
