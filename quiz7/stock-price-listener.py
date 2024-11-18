850a166f4db2fba7a080d27af4df86d15ae256c1
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-18 14:10:27 -0800

    Update stock-price-listener.py

--- quiz7/stock-price-listener.py
+++ quiz7/stock-price-listener.py
@@ -61,21 +61,20 @@ if __name__ == "__main__":
         .format("memory") \
         .start()
     
-    aapl_10.writeStream\
-        .queryName('aapl_10')\
+    msft_40.writeStream\
+        .queryName('msft_40')\
         .outputMode("complete")\
         .format("memory") \
         .start()
     
     time.sleep(5)
     while True:
-        rows_10 = spark.sql('select * from aapl_10').collect()
-        rows_40 = spark.sql('select * from aapl_40').collect()
-        
-        avg_date = rows_10[0]['max(date)']
-        a_avg_10 = rows_10[0]['avg(price)']
-        a_avg_40 = rows_40[0]['avg(price)']
+        aapl_rows_40 = spark.sql('select * from aapl_40').collect()
+        appl_avg_40 = aapl_rows_40[0]['avg(price)']
 
-        print(rows_40)
-        print(avg_date, a_avg_10, a_avg_40)
+        msft_rows_40 = spark.sql('select * from msft_40').collect()
+        msft_avg_40 = msft_rows_40[0]['avg(price)']
+
+        print(appl_avg_40, msft_avg_40)
+        
         time.sleep(3)
