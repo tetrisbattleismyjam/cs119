828bb55c77d56137050d8ba059f136c8af7f3d37
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-19 12:24:01 -0800

    Update stock-price-listener2.py

--- quiz7/stock-price-listener2.py
+++ quiz7/stock-price-listener2.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     while q.isActive:
             rows = spark.sql('select * from aapl10day').tail(1)
             if len(rows) > 0:
-                aapl10dayAvg = rows[1]['avg(price)']
+                aapl10dayAvg = rows[0]['avg(price)']
                 print(aapl10dayAvg)
             
     q.awaitTermination()
