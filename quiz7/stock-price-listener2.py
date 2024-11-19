34b9a9f8afc9befe7b936b19c047527d4c0e86f6
tetrisbattleismyjam <151405673+tetrisbattleismyjam@users.noreply.github.com>
2024-11-19 11:23:06 -0800

    Update stock-price-listener2.py

--- quiz7/stock-price-listener2.py
+++ quiz7/stock-price-listener2.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     aapl10Day = aaplPrices.groupBy(sql_f.window('date', '10 days', '2 days')).agg({'price': 'avg'}).sort('window')
     
     q = aapl10Day.writeStream\
-                .outputMode('Complete')\
+                .outputMode('Append')\
                 .option('truncate', False)\
                 .format('console')\
                 .start()
